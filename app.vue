4a808abb1ac727274a6fe4d4ddbfa1d46ac4159b
ZTL-UwU <zhangtianli2006@163.com>
2025-04-02 13:10:05 +0800

    chore: init
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>
+    Hello Nuxt Fonts!
+  </h1>
+</template>
+
+<style scoped>
+h1 {
+  font-family: 'Geist Mono', sans-serif;
+  font-size: 7em;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
