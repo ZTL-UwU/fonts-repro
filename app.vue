5fdca224737a6fd4e1ee7260b1303d6ca6bdea27
ZTL-UwU <zhangtianli2006@163.com>
2025-04-02 13:19:03 +0800

    chore: add tailwind
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- app.vue
+++ app.vue
@@ -1,12 +1,11 @@
 <template>
-  <h1>
+  <h1 class="font-mono">
     Hello Nuxt Fonts!
   </h1>
 </template>
 
 <style scoped>
 h1 {
-  font-family: 'Geist Mono', sans-serif;
   font-size: 7em;
   margin-top: 40px;
   text-align: center;
