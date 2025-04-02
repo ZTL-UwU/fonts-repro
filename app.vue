e933848e59c4122e9f6dbffb1c58304b9d48590e
ZTL-UwU <zhangtianli2006@163.com>
2025-04-02 13:24:54 +0800

    font weight?
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- app.vue
+++ app.vue
@@ -5,6 +5,24 @@
   <h1 class="font-mono">
     Hello Nuxt Fonts!
   </h1>
+  <h1 class="font-normal">
+    Hello Nuxt Fonts!
+  </h1>
+  <h1 class="font-semibold">
+    Hello Nuxt Fonts!
+  </h1>
+  <h1 class="font-medium">
+    Hello Nuxt Fonts!
+  </h1>
+  <h1 class="font-bold">
+    Hello Nuxt Fonts!
+  </h1>
+  <h1 class="font-light">
+    Hello Nuxt Fonts!
+  </h1>
+  <h1 class="font-black">
+    Hello Nuxt Fonts!
+  </h1>
 </template>
 
 <style scoped>
